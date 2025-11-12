3a46a93e54d96574a9bc091ea6bb277c230def26
Joe Bert <joebert@example.com>
2025-11-12 18:55:52 +0100

    Lower constellation max depth and refresh cache

--- scripts/database/generate_relationship_constellations_cache.py
+++ scripts/database/generate_relationship_constellations_cache.py
@@ -56,7 +56,7 @@ async def fetch_people(conn):
 
 async def generate_relationship_constellations_cache():
     """Generate JSON cache of relationship constellations between different political families"""
-    MAX_CHAIN_DEPTH = 3
+    MAX_CHAIN_DEPTH = 2
     load_env_from_dotenv()
     load_dotenv()
     
